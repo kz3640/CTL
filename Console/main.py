eec55e1ec678909d94d0f655220f3d3d787a693d
kz3640 <kz3640@rit.edu>
2023-11-13 13:00:01 -0500

    the program now returns the list of songs in the playlist with the command
    !play <> <playlist share link>

--- Console/main.py
+++ Console/main.py
@@ -4,84 +4,103 @@ import datetime
 from states import States
 from gamemodes import Gamemodes
 
-
 auth_url = 'https://accounts.spotify.com/api/token'
 client_id = None
 client_secret = None
 token = None
 genTime = None
-playlist = None
+playlist_json = None
 
-#load secrets
-with open('secrets') as f:
-    client_id = f.readline().strip()
-    client_secret = f.readline().strip()
-auth_headers = {
-    'Authorization': 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode('utf-8')).decode('utf-8')
-}
-auth_data = {
-    'grant_type': 'client_credentials'
-}
-playlist_headers = {
-    'Authorization': 'Bearer ' + ""
-}
 
-def genToken():
+# Load secrets
+def load_secrets():
+    global client_id, client_secret
+    with open('secrets') as f:
+        client_id = f.readline().strip()
+        client_secret = f.readline().strip()
+
+
+def generate_token():
+    auth_headers = {
+        'Authorization': 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode('utf-8')).decode('utf-8')
+    }
+    auth_data = {
+        'grant_type': 'client_credentials'
+    }
     auth_response = requests.post(auth_url, headers=auth_headers, data=auth_data)
+
     if auth_response.status_code == 200:
-        global token
-        global genTime
+        global token, genTime
         token = auth_response.json().get('access_token')
         genTime = datetime.datetime.now()
-        updateHeaders()
+        update_headers()
     else:
         print('Error:', auth_response.status_code, auth_response.text)
 
-def updateHeaders():
+
+def update_headers():
     global playlist_headers
     playlist_headers = {
         'Authorization': 'Bearer ' + token
     }
 
-def fetchID(playlist_id):
-    url = "https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks?offset=0&limit=100"
-    r = requests.get(url, headers=playlist_headers)
-    return r
+
+def fetch_id(playlist_id):
+    #fetches json data from spotify api
+    url = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks?offset=0&limit=100"
+    response = requests.get(url, headers=playlist_headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print('Error:', response.status_code, response.text)
+        return None
+
+
 def play(mode, url):
     last_slash_index = url.rfind("/")
-    playlist_id = url[last_slash_index + 1 : url.find("?", last_slash_index)]
-    global playlist
-    playlist = fetchID(playlist_id)
-    print("ERROR CODE " + str(playlist.status_code))
-    mode = mode.lower()
-    return States.FREEINPUT#TODO ACTUALLY IMPLEMENT THIS, THIS IS PLACEHOLDER
-    if mode == "CTL":
-        return Gamemodes.CTL
+    playlist_id = url[last_slash_index + 1: url.find("?", last_slash_index)]
 
-def handleFreeInput(user_input):
+    playlist_data = fetch_id(playlist_id)
+
+    if playlist_data:
+        global playlist_json
+        playlist_json = playlist_data
+
+        songs_array = playlist_json['items']
+        for x in songs_array:
+            print(x['track']['name'])
+
+        mode = mode.lower()
+        return States.FREEINPUT  # TODO ACTUALLY IMPLEMENT THIS, THIS IS PLACEHOLDER
+        if mode == "ctl":
+            return Gamemodes.CTL
+    else:
+        # Handle the case where fetching playlist data fails
+        return States.FREEINPUT
+
+
+def handle_free_input(user_input):
     user_input_lower = user_input.lower()
     if user_input_lower == "!help":
+        # Handle help command logic
         print("!help - Shows all commands.")
         print("!quit - Exits the program.")
         print("!settings - Shows settings")
         return States.FREEINPUT
-        # Handle help command logic
 
     elif user_input_lower == "!quit":
+        # Handle quit command logic
         print("Quitting the program...")
         return States.QUIT
-        # Handle quit command logic
 
     elif user_input_lower == "!settings":
+        # Handle settings command logic
         print("Opening settings...")
         return States.SETTINGSMENU
-        # Handle settings command logic
 
     elif user_input_lower.startswith("!play"):
-        # Split the input into command and arguments
         _, mode, url = user_input.split(maxsplit=2)
-
-        # Handle the play command with mode and URL
         play_response = play(mode, url)
         return play_response
 
@@ -90,17 +109,20 @@ def handleFreeInput(user_input):
 
     return
 
-def handleSettingsMenu():
+
+def handle_settings_menu():
+    # Add logic for handling settings menu
     return
+
+
 def main():
-    genToken()
+    load_secrets()
+    generate_token()
     current_state = States.FREEINPUT
-    playlist_url = ""
-    while(current_state != States.QUIT):
-        if(current_state == States.FREEINPUT):
-            current_state = handleFreeInput(input("Enter a command."))
 
-
+    while current_state != States.QUIT:
+        if current_state == States.FREEINPUT:
+            current_state = handle_free_input(input("Enter a command."))
 
 
 if __name__ == "__main__":
